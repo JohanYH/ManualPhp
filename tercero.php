41b6c31100e212c8f3599207f6cd2b5eb893078c
JohanYH <johanpuerto2005@gmail.com>
2023-06-07 07:47:19 -0500

    Manual Php

--- tercero.php
+++ tercero.php
@@ -61,4 +61,145 @@ if (10 > 3) {
     //Si no cumple la accion
 }
 
+$autenticado = true;
+$admin = false; //Como es false, va votar que no es autentico
+
+if ($autenticado && $admin) {
+    echo "Usuario autenticado correctamente";
+}else {
+    echo "Usuario no autenticado, inicia sesion";
+}
+echo"<br> <br>";
+
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 0,
+    'informacion' => [
+        'tipo' => 'Premium'
+    ]
+];
+
+echo"<br> <br>";
+
+//Un ejemplo de If con Else
+
+if (!empty($cliente)) {
+    echo 'El Arreglo de cliente no esta vacio <br>';
+    if ($cliente['saldo']> 0) {
+        echo 'El Cliente tiene saldo disponible <br> <br>';
+    }else {
+        echo 'No hay saldo <br> <br>';
+    }
+};
+
+if ($cliente['saldo']> 0) {
+    echo 'El Cliente tiene saldo';
+}elseif ($cliente['informacion']['tipo']=== 'Premium') { //Soltara elseif por que if no cumple la condicion y elseif si, por esto tampoco va a votar else
+    echo 'El cliente es Premium';
+}else {
+    echo 'No hay Cliente definido o no tiene saldo o no es Premium';
+};
+
+echo"<br> <br>";
+
+$tecnologia = 'HTML'; // Va leer el case que tiene la variable y si estuviera vacia leeria default, o si es diferente la variable
+
+switch ($tecnologia) {
+    case 'PHP':
+        echo 'PHP, un execelente lenguaje';
+        break;
+    case 'JavaScript':
+        echo 'Genial, el lenguaje de la web';
+        break;
+    case 'HTML':
+        echo 'Emm..';
+        break;
+    default:
+        echo 'Algun lenguaje que no se cual es';
+        break;
+};
+
+echo"<br> <br>";
+
+//El Bucle while, esto permite repetir la ejecucion de un bloque de codigo mientras esto cumpla la condicion
+
+//El Bucle do-while, esto es similar al bucle while, pero esto garantiza que el bloque de codigo se ejecuta al menos una vez, independientemente si cumple o no la condicion
+
+//Bucle for, esto permite la ejecucion de un bloque de codigo un numero fijo de veces, se puede controlar el numero de iteraciones mediante un contador
+
+//Ejemplo de While
+
+$i = 0;
+
+while ($i < 10) {
+    echo $i . "<br>";
+
+    $i++; //El "++" incrementa la variable
+}
+
+echo"<br> <br>";
+
+//Ejemplo de Do While
+
+$a = 100;
+
+do {
+    echo $a . '<br>';
+
+    $a++;
+} while ($a < 10);
+
+echo"<br> <br>";
+
+//Ejemplo de For Each
+
+$clientes = array('Pedro', 'Juan', 'Karen');
+
+foreach($clientes as $cliente):
+    echo $cliente . '<br/>';
+endforeach;
+
+echo"<br> <br>";
+
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200, 
+    'tipo' => 'Premium'
+];
+
+foreach ($cliente as $key => $valor):
+    echo $key . ' - ' . $valor . '<br/>';
+endforeach;
+
+$productos = [
+    [
+        'nombre' => 'Celular',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Television 24',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvo',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
+
+echo"<br> <br>";
+
+foreach($productos as $producto){?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre'];?></p>
+        <p>Precio: <?php echo "$" . $producto['precio'];?></p>
+        <p><?php echo ($producto['disponible'])? 'Disponible' : 'No Disponible';?></p>
+    </li>
+<?php
+}
+
+
+
 ?>
